0d74b25451d609a0be341c8632fb6efdc13d277d
Luigi T <luigitejada@yahoo.com>
2022-09-20 09:23:08 -0400

    add students

---
+++ data/students.js
@@ -0,0 +1,10 @@
+const students = [
+  {text: 'Michael Jordan', basketball_team: true, _id: 23},
+  {text: 'Kobe Bryant', basketball_team: true, _id: 24},
+  {text: 'Lebron James', basketball_team: true, _id: 6},
+  {text: 'Luigi Tejada', basketball_team: false, _id: 1},
+]
+
+export { 
+	students
+}
